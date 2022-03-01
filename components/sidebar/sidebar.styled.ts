911eeb1447cb04d70452c7dfa876258eb9d42989
HongBeen <redbean-2@naver.com>
2022-03-02 01:40:17 +0900

    add sidebar slide

--- components/sidebar/sidebar.styled.ts
+++ components/sidebar/sidebar.styled.ts
@@ -1,20 +1,32 @@
 import styled from "styled-components";
 
-const ICON_SIZE = 40;
+const ICON_SIZE = 32;
 
-export const IconContainerList = styled.ul`
+export const IconList = styled.ul`
   position: absolute;
   top:0;
   left:100%;
   transform: translateX(-100%);
+  padding: 10px;
   display: flex;
-  flex-direction: row-reverse;
   justify-content: flex-end;
 `;
 
-export const IconContainer = styled.li`
+export const Icon = styled.div`
   width:${ICON_SIZE}px;
   height:${ICON_SIZE}px;
+  padding:2px;
+  cursor: pointer;
+`;
+
+export const MenuIcon = styled(Icon)``;
+
+export const SettingIcon = styled(Icon)`
+  position: fixed;
+  top:0;
+  left:100%;
+  transform: translateX(-100%);
+  padding: 10px 10px 0 0;
 `;
 
 export const MenuOverlay = styled.div`
@@ -25,17 +37,63 @@ export const MenuOverlay = styled.div`
   width:100%;
   height:100%;
   background-color: rgba(0,0,0,0.5);
+  visibility: ${props => props.hidden ? "hidden" : "visible"};
 `;
 
 export const MenuContents = styled.section`
-  z-index: 11;
   display: flex;
   flex-direction: column;
   position: absolute;
   top:0;
   left:100%;
   transform: translateX(-100%);
-  width: 260px;
+  width: 280px;
   height:100%;
+  padding:20px;
   background-color: ${props => props.theme.colors["gray-800"]};
+`;
+
+export const Profile = styled.div`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  font-size: 1rem;
+  font-weight: 500;
+  padding: 15px 0 10px 0;
+  border-top: 1px solid rgba(255,255,255,0.1);
+`;
+
+export const Name = styled.div`
+  font-size: 1.8rem;
+  font-weight: 900;
+  margin-bottom: 10px;
+`;
+
+export const Email = styled.div`
+  font-size: 0.8rem;
+  margin-bottom: 20px;
+`;
+
+export const FriendList = styled.div`
+  margin: 20px 0;
+`;
+
+export const Friend = styled.div`
+  font-size: 1rem;
+  margin-right: 10px;
+`;
+
+export const CategoryList = styled.ul``;
+
+export const Category = styled.li`
+  width:fit-content;
+  background-color: rgba(255,255,255,0.1);
+  padding: 11px;
+  border-radius: 5px;
+  margin-top: 5px;
+  font-size: 0.8rem;
+  font-weight: 500;
 `;
--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,31 +1,44 @@
-import {Menu,Save} from "@styled-icons/entypo"
-import React, {MouseEventHandler, useState} from "react"
+import {Settings,Menu} from "@styled-icons/feather";
+import React, {useState} from "react";
 import * as S from "./sidebar.styled";
 
-type OnClick = (event:Event) => void;
-
 export const Sidebar = () => {
-  const [menuHidden,setMenuHidden] = useState(true);
+  const [menuHidden, setMenuHidden] = useState(true);
 
-  const handleHiddenMenu = (event?:Event)=>{
-    if(event && event.target !== event.currentTarget) return;
+  const handleHiddenMenu = (
+    event: React.MouseEvent<HTMLElement> | React.MouseEvent<SVGSVGElement>
+  ) => {
+    if (event.target !== event.currentTarget) return;
     setMenuHidden(!menuHidden);
-  }
+  };
 
-  return <>
-    { menuHidden ? 
-      (<S.IconContainerList>
-        <S.IconContainer onClick={()=>handleHiddenMenu(event)}>
-          <Menu/>
-        </S.IconContainer>
-      </S.IconContainerList>) : 
-      (<S.MenuOverlay onClick={()=>handleHiddenMenu(event)}>
+  return (
+    <>
+      <S.IconList>
+        <S.MenuIcon>
+          <Menu onClick={handleHiddenMenu} />
+        </S.MenuIcon>
+      </S.IconList>
+      <S.MenuOverlay onClick={handleHiddenMenu} hidden={menuHidden}>
         <S.MenuContents>
-          <h4>menu1</h4>
-          <h4>menu2</h4>
-          <h4>menu3</h4>
+          <S.SettingIcon>
+            <Settings/>
+          </S.SettingIcon>
+          <S.Profile>
+            <S.Name>HongBeen Lee</S.Name>
+            <S.Email>redbean-2@naver.com</S.Email>
+            <S.FriendList>
+              <S.Friend>{"2 팔로워"}</S.Friend>
+              <S.Friend>{"2 팔로잉"}</S.Friend>
+            </S.FriendList>
+          </S.Profile>
+          <S.CategoryList>
+            <S.Title>목표</S.Title>
+            <S.Category>Project</S.Category>
+            <S.Category>Algorithm</S.Category>
+          </S.CategoryList>
         </S.MenuContents>
-      </S.MenuOverlay>)
-    }
-  </>
-}
+      </S.MenuOverlay>
+    </>
+  );
+};
