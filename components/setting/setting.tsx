66ef28dc57fa5b3eab9b664587c9744f6a7c9eee
hotbreakb <areyhappy12@gmail.com>
2022-03-05 14:45:59 +0900

    add setting props

--- components/setting/setting.tsx
+++ components/setting/setting.tsx
@@ -2,22 +2,28 @@ import React from "react";
 import * as S from "./setting.styled";
 import { SliderSwitch } from "./sliderSwitch";
 
-export const Setting = () => {
+type SettingProps = {
+  email: string;
+  profile: string;
+  id: string;
+};
+
+export const Setting = ({ email, profile, id }: SettingProps) => {
   return (
     <S.infoList>
       <S.infoListItemEmail>
         <S.SubHeading>연동된 이메일</S.SubHeading>
-        <S.email>ghdqlsdl9633@gmail.com</S.email>
+        <S.email>{email}</S.email>
       </S.infoListItemEmail>
 
       <S.infoListItem>
         <S.SubHeading>프로필</S.SubHeading>
-        <S.name>귀신</S.name>
+        <S.name>{profile}</S.name>
       </S.infoListItem>
 
       <S.infoListItem>
         <S.SubHeading>사용자 아이디</S.SubHeading>
-        <S.name>Hongbeen</S.name>
+        <S.name>{id}</S.name>
       </S.infoListItem>
 
       <S.infoListItem>
