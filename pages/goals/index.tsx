1eadd5224919b01c01ef7b64fcad553b03555fbd
hotbreakb <areyhappy12@gmail.com>
2022-03-07 08:08:33 +0900

    add chip in goals page

--- pages/goals/index.tsx
+++ pages/goals/index.tsx
@@ -1,10 +1,13 @@
 import type {NextPage} from "next";
 import {useRouter} from "next/router";
-import React from "react";
+import React, {useState} from "react";
 import {LayoutNavigation} from "components/layout-navigation";
+import {Chip} from "components/chip";
 
 const GoalsPage: NextPage = () => {
   const router = useRouter();
+  const [goals, setGoals] = useState(["Algorithm", "Personal"]); //TODO: 지우기
+  const [goalsColor, setGoalsColor] = useState(["orange", "blueviolet"]); //TODO: 지우기
 
   const onClick = () => {
     router.push("/");
@@ -16,7 +19,14 @@ const GoalsPage: NextPage = () => {
       rightButtonText="+"
       onLeftButtonClick={onClick}
     >
-      Goal입니다
+      {goals.map((value, index) => (
+        <Chip
+          key={index}
+          label={value}
+          color={goalsColor[index]}
+          onAdd={() => console.log(`${value} clicked`)}
+        ></Chip>
+      ))}
     </LayoutNavigation>
   );
 };
