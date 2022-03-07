d14f98e239fa587bf980df7b4629ef4d82be684f
hotbreakb <areyhappy12@gmail.com>
2022-03-07 10:16:48 +0900

    add goals component

---
+++ components/goals/goals.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SubHeadingContainer = styled.div`
+margin: 0.8rem 0 0.8rem 0;
+`
+
+export const FeedContents = styled.ul`
+  flex-direction: column;
+  > * {
+    margin-bottom: 20px;
+  }
+`;
---
+++ components/goals/goals.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import * as S from "./goals.styled";
+import {Chip} from "components/chip";
+import {SubHeadingSpan} from "components/subHeading/subHeadingSpan";
+
+export const Goals = () => {
+  const [goals, setGoals] = useState(["Algorithm", "Personal"]); //TODO: 지우기
+  const [goalsColor, setGoalsColor] = useState(["orange", "blueviolet"]); //TODO: 지우기
+
+  return (
+    <>
+      <S.SubHeadingContainer>
+        <SubHeadingSpan>일반</SubHeadingSpan>
+      </S.SubHeadingContainer>
+
+      <S.FeedContents>
+        {goals.map((value, index) => (
+          <Chip key={index} label={value} color={goalsColor[index]}></Chip>
+        ))}
+      </S.FeedContents>
+    </>
+  );
+};
