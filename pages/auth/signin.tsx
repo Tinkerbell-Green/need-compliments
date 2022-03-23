b73ce3f2433cef62f147ae7617ac93a9be4ac559
jaehyeon(wiz) <eiirwp@gmail.com>
2022-03-23 18:15:03 +0900

    fix revert redirect in signIn page

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -13,6 +13,15 @@ import {LayoutCenter} from "components/templates/layout-center";
 export default function SignIn({
   providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const {status} = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.replace("/");
+    }
+  }, [router, status]);
+   
   return (
     <LayoutCenter>
       {providers
