4d5a3d48c2a594236a7950a1c444887186b434ac
HongBeen <redbean-2@naver.com>
2022-03-23 22:14:37 +0900

    fix:type

--- components/goals/goals.tsx
+++ components/goals/goals.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import * as S from "./goals.styled";
 import {Chip} from "components/atoms/chip";
 import {SubHeadingSpan} from "components/subHeading/subHeadingSpan";
 
