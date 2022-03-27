55cb8062ccee16a60da891c7097c8e2e91f3e7c4
HongBeen <redbean-2@naver.com>
2022-03-27 17:58:04 +0900

    refactor: feedItem prop type

--- components/moleculs/feedItem/feedItem.tsx
+++ components/moleculs/feedItem/feedItem.tsx
@@ -2,42 +2,35 @@ import React,{useState} from "react";
 import * as S from "./feedItem.styled";
 import {Chip} from "components/atoms/chip";
 import {IconThumbsUp} from "components/moleculs/iconThumbsUp";
-import {GoalColor} from "stores/data";
-// TODO: dayjs('2019-01-25').format('MM/DD HH:mm')
+import {GoalData, TaskData} from "stores/data";
+import {Dayjs} from "utils/dayjs"
+
 type FeedItemProps = {
-  goal? : string,
-  goalColor?: GoalColor,
-  task?: string,
-  complimentsCount?: number,
-  writer?: string,
-  doneAt?: string,
-};
+  task: TaskData,
+  goal: GoalData
+}
 
 export const FeedItem = ({
-  goal="개발왕",
-  goalColor="mediunslateblue",
-  task="칭필을 개발개발",
-  complimentsCount=17,
-  writer="홍빈이",
-  doneAt="03/27 16:21",
+  task,
+  goal,
 }: FeedItemProps) => {
   const [isThumbsClick, setIsThumbsClick] = useState(false);
 
   return (
     <li>
       <S.Item>
-        <Chip label={goal} color={goalColor}></Chip>
-        <S.Task>{task}</S.Task>
+        <Chip label={goal.name} color={goal.color}></Chip>
+        <S.Task>{task.title}</S.Task>
         <div>
           <S.Reaction onClick={()=>{
             setIsThumbsClick(true);
             setTimeout(()=>setIsThumbsClick(false),2000);
           }}>
             <IconThumbsUp clicked={isThumbsClick}></IconThumbsUp>
-            <S.Count>{complimentsCount}</S.Count>
+            <S.Count>{task.compliments.length}</S.Count>
           </S.Reaction>
-          <S.Li>{writer}</S.Li>
-          <S.Li>{doneAt}</S.Li>
+          <S.Li>{task.author}</S.Li>
+          <S.Li>{Dayjs(task.doneAt).format("MM/DD HH:mm")}</S.Li>
         </div>
       </S.Item>
     </li>
