94e34de9e02ebf010c880eadc9b0fa7df12d7175
HongBeen <redbean-2@naver.com>
2022-04-22 11:40:02 +0900

    add: notice

--- components/organisms/feedNotice/feedNotice.tsx
+++ components/organisms/feedNotice/feedNotice.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import * as S from "./feedNotice.styled";
+import {Chip} from "components/atoms/chip";
+import * as S from "components/moleculs/feedItem/feedItem.styled";
 
-const Notice=["나의 일상을 칭찬으로 기록해보세요! 🐋🐋🐋","리액션 카운트 기능이 추가될 예정입니다.","하루에 5개를 기록하면 달을 볼 수 있어요!"]
+const Notice=[
+  "나의 일상을 칭찬으로 기록하는 칭찬이필요해 입니다.",
+  "리액션 카운트 기능이 추가되었습니다. 로그인하여 사용해보세요!",
+  "하루에 5개 칭찬을 기록하면 달력에서 달🌙을 볼 수 있어요"]
 
 export const FeedNotice = () => {
   return (
-    <S.Feed>
-      {Notice.map((value)=>(
-        <S.Li key={value}><p>{value}</p></S.Li>
-      ))}
-    </S.Feed>
+    Notice.map((value)=>(
+      <S.Item key={value}>
+        <S.Goal><Chip label={"공지"} color={"mediunslateblue"}></Chip></S.Goal>
+        <S.Task>{value}</S.Task>
+        <S.Li>{"04/22 11:36"}</S.Li>
+      </S.Item>
+    ))
   );
 };
