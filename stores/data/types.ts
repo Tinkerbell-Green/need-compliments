4adbf26ebcebfb60f8124b97bd80bec7ee107941
ghtea <eiirwp@gmail.com>
2022-05-30 19:08:54 +0900

    remove unnecessary comment

--- stores/data/types.ts
+++ stores/data/types.ts
@@ -1,56 +1,5 @@
 import {DataActionType} from "./actions"
 
-// export type TaskDocument = {
-//   title: string
-//   goal: string
-//   author: string
-//   doneAt: number
-//   createdAt: number
-//   updatedAt: number
-//   readPermission: "everyone" | "me" | "none"
-// }
-// export type TaskData = TaskDocument & {
-//   id: string
-//   compliments: ComplimentData[]
-// }
-
-// export type GoalColor = keyof Theme["colors"]["goals"]
-// export type GoalDocument = {
-//   name: string
-//   color: GoalColor
-//   readPermission: "everyone" | "me" | "none"
-//   author: string
-//   createdAt: number
-//   updatedAt: number
-// }
-// export type GoalData = GoalDocument & {
-//   id: string
-// }
-
-// export type ComplimentDocument = {
-//   type: "party-popper" | "thumbs-up" | "clapping-hands" | "red-heart"
-//   task: string
-//   author: string
-//   createdAt: number
-//   updatedAt: number
-// }
-// export type ComplimentData = ComplimentDocument & {
-//   id: string
-// }
-
-// export type UserDocument = {
-//   email: string
-//   name: string
-//   image: string
-//   followers: string[]
-//   followings: string[]
-//   createdAt: number
-//   updatedAt: number
-// }
-// export type UserData = UserDocument & {
-//   id: string
-// }
-
 export enum DataSagaStatus {
   LOADING = "loading",
   FAILED = "failed",
