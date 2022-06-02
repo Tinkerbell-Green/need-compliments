bb666353731adb09516eedcf3caa9f87226cfa2e
HongBeen <redbean-2@naver.com>
2022-06-02 18:26:21 +0900

    style: opacity transition

--- components/moleculs/snackbarAnimation/snackbarAnimation.styled.tsx
+++ components/moleculs/snackbarAnimation/snackbarAnimation.styled.tsx
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const Snackbarify = styled.div<{isVisible:boolean}>`
 z-index:10;
 position: fixed;
-top: 8%;
+top: 20px;
 left: 50%;
 transition: all 0.4s ease-out;
-visibility: ${props=>props.isVisible ? "visible" : "hidden"};
+opacity: ${props=>props.isVisible ? "1" : "0"};
 transform: ${props=>props.isVisible ? "translate(-50%, 0%);" : "translate(-50%, -300%);"};
 `
--- pages/test/snackbar/index.tsx
+++ pages/test/snackbar/index.tsx
@@ -1,36 +1,42 @@
 import {NextPage} from "next";
 import {useCallback} from "react";
-import {Snackbar,SnackbarProps} from "components/atoms/snackbar";
+import {Snackbar} from "components/atoms/snackbar";
 import {SnackbarAnimation} from "components/moleculs/snackbarAnimation";
 import {useSnackbar} from "utils/snackbarify/snackbarHook";
 
-const SnackbarTestPage: NextPage = ()=>{
-  const {isSnackbarVisible, setIsSnackbarVisible, snackbarDuration} = useSnackbar();
+const SnackbarTestPage: NextPage = () => {
+  const {isSnackbarVisible, setIsSnackbarVisible, snackbarDuration} =
+		useSnackbar();
 
-  const onSnackbarShowClick = useCallback(()=>{
-    setIsSnackbarVisible({visible:true});
-  },[setIsSnackbarVisible])
+  const onSnackbarShowClick = useCallback(() => {
+    setIsSnackbarVisible({visible: true});
+  }, [setIsSnackbarVisible]);
 
-  const onSnackbarHideClick = useCallback(()=>{
-    setIsSnackbarVisible({visible:false})
-  },[setIsSnackbarVisible])
-
-  const snackbarProps:SnackbarProps = {
-    message: "This is snackbar!",
-    type: "information",
-    duration: snackbarDuration,
-    onCloseClick: onSnackbarHideClick
-  }
+  const onSnackbarHideClick = useCallback(() => {
+    setIsSnackbarVisible({visible: false});
+  }, [setIsSnackbarVisible]);
 
   return (
     <>
       <SnackbarAnimation
         visible={isSnackbarVisible}
-        Snackbar={()=><Snackbar {...snackbarProps}></Snackbar>}></SnackbarAnimation>  
-      <button style={{margin: "150px 20px"}} onClick={onSnackbarShowClick}>SHOW SNACKBAR</button>
-      <button style={{margin: "150px 20px"}} onClick={onSnackbarHideClick}>HIDE SNACKBAR</button>
+        Snackbar={() => (
+          <Snackbar
+            message="This is snackbar!"
+            type="information"
+            duration={snackbarDuration}
+            onCloseClick={onSnackbarHideClick}
+          ></Snackbar>
+        )}
+      ></SnackbarAnimation>
+      <button style={{margin: "150px 20px"}} onClick={onSnackbarShowClick}>
+				SHOW SNACKBAR
+      </button>
+      <button style={{margin: "150px 20px"}} onClick={onSnackbarHideClick}>
+				HIDE SNACKBAR
+      </button>
     </>
   );
-}
+};
 
 export default SnackbarTestPage;
