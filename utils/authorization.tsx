4de642ac39a93b937c097bbdfd97a6683b3a975a
HongBeen <redbean-2@naver.com>
2022-06-18 20:14:27 +0900

    add: redirction setting

--- utils/authorization.tsx
+++ utils/authorization.tsx
@@ -12,7 +12,7 @@ type AuthorizationProviderProps = {
   children: ReactNode
 }
 
-const PUBLIC_PAGE_PATHNAMES = ["/auth/signin","/","/test"]
+const PUBLIC_PAGE_PATHNAMES = ["/auth/signin","/test"]
 
 export const AuthorizationProvider = ({
   children
@@ -21,17 +21,23 @@ export const AuthorizationProvider = ({
   const {status, data: session} = useSession()
   const dispatch = useDispatch()
   const {data} = useDataSaga<DataActionType.GET_LOGGED_IN_USER_DATA>(DataActionType.GET_LOGGED_IN_USER_DATA, [])
+  const redirection = useMemo(()=>{
+    const currentPath = router.pathname;
+      
+    if (currentPath !== "/" && !PUBLIC_PAGE_PATHNAMES.filter(path => currentPath.includes(path)).length) return true;
+    return false;
+  },[router.pathname])
 
   useEffect(() => {
     if (status === "unauthenticated") {
-      if (!PUBLIC_PAGE_PATHNAMES.filter(path => router.pathname.includes(path))){
-        router.replace("/");
+      if(redirection){
+        router.replace("/")
       }
       dispatch(navigationActionCreators[NavigationActionType.SET_INITIALIZED]({
         initialized: true
       }))   
     }
-  }, [router,status,dispatch]);
+  }, [router,status,dispatch,redirection]);
 
   useEffect(()=>{
     if (status === "authenticated" && data?.user.userId){
@@ -46,7 +52,6 @@ export const AuthorizationProvider = ({
     }
   }, [data?.user.userId, dispatch, session, status])
 
-  return (
-    <>{children}</>
-  )
+  if(status === "loading" && redirection) return (<LayoutCenter><Spinner></Spinner></LayoutCenter>);
+  return <>{children}</>
 }
