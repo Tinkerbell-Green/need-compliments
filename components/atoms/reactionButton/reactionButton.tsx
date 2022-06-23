d2f527b7234c9414ffd5ecde1743421e06405535
HongBeen <redbean-2@naver.com>
2022-06-23 15:56:47 +0900

    Add: reactionButton component

--- components/atoms/reactionButton/reactionButton.tsx
+++ components/atoms/reactionButton/reactionButton.tsx
@@ -1,12 +1,31 @@
-import React from "react"
+import React, {useCallback} from "react"
 import * as S from "./reactionButton.styled";
+import {ComplimentData} from "api"
 
-export const ReactionButton = () => {
+type ReactionButtonProps = {
+  type: ComplimentData["type"],
+  clicked: boolean,
+  onClickEmoji: (emoji: ComplimentData["type"]) => void;
+}
+
+const COMPLIMENTS_ICON_MAP:Record<ComplimentData["type"],string>={
+  "thumbs-up": "👍🏻",
+  "clapping-hands" : "👏🏻",
+  "party-popper": "🎉",
+  "red-heart": "❤️"
+}
+
+export const ReactionButton = ({type,clicked,onClickEmoji}:ReactionButtonProps) => {
+
+  const handleClickedEmoji=useCallback(()=>{
+    onClickEmoji(type);
+  },[onClickEmoji,type])
+
   return (
-    <S.Reaction 
-      onClick={()=>handleClickedEmoji("thumbs-up")} 
-      complimented={complimented?.type==="thumbs-up"}>
-      {"👍🏻"}
+    <S.Reaction
+      onClick={handleClickedEmoji} 
+      clicked={clicked}>
+      {COMPLIMENTS_ICON_MAP[type]}
     </S.Reaction>
   )
 }
