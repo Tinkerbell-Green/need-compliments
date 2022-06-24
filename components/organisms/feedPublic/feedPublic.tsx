ab62c0bcf15173b4a6b4d62de05d665dacb724b0
HongBeen <redbean-2@naver.com>
2022-06-25 04:21:06 +0900

    fix: feedPublic animation state, props sent

--- components/organisms/feedPublic/feedPublic.tsx
+++ components/organisms/feedPublic/feedPublic.tsx
@@ -1,20 +1,42 @@
-import React from "react";
-import * as S from "./feedPublic.styled";
-import {GetTasksData} from "api"
+import React,{useCallback, useState} from "react";
+import {GetTasksData,ComplimentType} from "api"
 import {FeedItem} from "components/moleculs/feedItem"
+import {IconHeart} from "components/moleculs/iconHeartBeat";
 
 type FeedPublicProps = {
   tasks: GetTasksData["tasks"];
+  loggedInUserId:string | null,
+  onComplimentCreate: (emoji: ComplimentType, taskId:string)=>void,
+  onComplimentDelete: (complimentId:string)=>void,
 };
 
 export const FeedPublic = ({
   tasks,
+  loggedInUserId,
+  onComplimentCreate,
+  onComplimentDelete
 }: FeedPublicProps) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleAnimationHide = useCallback(()=>setIsClicked(false),[setIsClicked]);
+  const handleAnimationShow = useCallback(()=>setIsClicked(true),[setIsClicked]);
   return (
-    <S.Feed>
-      {(tasks || []).map(item => (
-        <FeedItem key={item._id} task={item} goal={item.goalData}></FeedItem>
-      ))}
-    </S.Feed>
+    <>
+      <ul>
+        {(tasks || []).map(item => (
+          <FeedItem 
+            key={item._id} 
+            task={item} 
+            goal={item.goalData}
+            loggedInUserId={loggedInUserId}
+            onComplimentCreate={onComplimentCreate}
+            onComplimentDelete={onComplimentDelete}
+            onAnimationShow={handleAnimationShow}
+          ></FeedItem>
+        ))}
+      </ul>
+      {isClicked && <IconHeart
+        isVisible={isClicked}
+        onHide={handleAnimationHide}></IconHeart>}
+    </>
   );
 };
