5a966dc647a50b6a6ae875790a0869abeec94774
HongBeen <redbean-2@naver.com>
2022-06-27 00:21:14 +0900

    refactor: split function

--- components/moleculs/feedItem/feedItem.tsx
+++ components/moleculs/feedItem/feedItem.tsx
@@ -5,8 +5,8 @@ import * as S from "./feedItem.styled";
 import {TaskData} from "api"
 import {ComplimentData, GoalData,ComplimentType} from "api"
 import {SnackbarProps} from "components/atoms/snackbar";
+import {FeedItemCompliment} from "components/moleculs/feedItemCompliment";
 import {FeedItemGoal} from "components/moleculs/feedItemGoal";
-import {FeedItemReactionList} from "components/moleculs/feedItemReactionList";
 import {FeedItemTask} from "components/moleculs/feedItemTask";
 import {FeedItemTime} from "components/moleculs/feedItemTime";
 import {useSnackbarifyState} from "utils/snackbarify"
@@ -32,44 +32,56 @@ const FeedItem = ({
   const clicked = useMemo(()=>{
     return task.compliments.find(compliment => compliment.author === loggedInUserId);
   },[task.compliments,loggedInUserId])
+  const [clickedReactionType, setClickedReactionType] = useState(clicked?.type);
+
+  const handleComplimentDelete = useCallback(()=>{
+    if(clicked) {
+      setClickedReactionType(undefined)
+      onComplimentDelete(clicked._id);
+    }
+  },[clicked,onComplimentDelete])
+
+  const handleComplimentCreate = useCallback((emoji:ComplimentData["type"])=>{
+    onAnimationShow();
+    setClickedReactionType(emoji)
+    onComplimentCreate(emoji,task._id);
+  },[onAnimationShow,task._id,onComplimentCreate])
 
   const handleSnackbarShow = useCallback(()=>{
+    const newProps:SnackbarProps = {
+      message: `로그인 후 "${task.title}" 를 칭찬할 수 있습니다.`,
+      type:"information",
+    };
+    setSnackbarProps(newProps);
     setIsSnackbarVisible(true)
-  },[setIsSnackbarVisible])
+  },[setIsSnackbarVisible,setSnackbarProps,task.title])
 
   const handleClickedEmoji = useCallback((emoji:ComplimentData["type"])=>{
     if(!loggedInUserId) {
-      const newProps:SnackbarProps = {
-        message: `로그인 후 "${task.title}" 를 칭찬할 수 있습니다.`,
-        type:"information",
-      };
-      setSnackbarProps(newProps);
       handleSnackbarShow();
       return;
     }
 
-    if(clicked) {
-      onComplimentDelete(clicked._id);
-      if(clicked.type === emoji) return;
-    }
+    handleComplimentDelete();
 
-    onAnimationShow()
-    onComplimentCreate(emoji,task._id);
+    if(clickedReactionType !== emoji){
+      handleComplimentCreate(emoji);
+    }
     
-  },[onComplimentDelete,task,clicked,setSnackbarProps,handleSnackbarShow,onComplimentCreate,loggedInUserId,onAnimationShow])  
+  },[clickedReactionType,handleSnackbarShow,loggedInUserId,handleComplimentDelete,handleComplimentCreate])  
 
   return (<>
     <li>
       <S.Item>
-        <FeedItemGoal label={goal?.name || ""} color={goal?.color || "white"}></FeedItemGoal>
-        <FeedItemTask title={task.title}></FeedItemTask>
+        {goal && <FeedItemGoal goal={goal}></FeedItemGoal>}
+        <FeedItemTask task={task}></FeedItemTask>
         <S.Info>
-          <FeedItemReactionList
+          <FeedItemCompliment
+            compliments={task.compliments}
             onEmojiClick={handleClickedEmoji}  
-            complimentsNumber={task.compliments.length}
-            clickedType={clicked ? clicked.type : null}
-          ></FeedItemReactionList>
-          <FeedItemTime time={task.createdAt}></FeedItemTime>
+            clickedType={clickedReactionType ? clickedReactionType : null}
+          ></FeedItemCompliment>
+          <FeedItemTime task={task}></FeedItemTime>
         </S.Info>
       </S.Item>
     </li>
